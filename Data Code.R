381fe4add79b36157e8a964a799290cb5fff5cef
erinor02 <s2214623@ed.ac.uk>
2024-11-27 12:12:00 +0000

    Converting data to numeric

--- Data Code.R
+++ Data Code.R
@@ -2,6 +2,8 @@
 #Erin O'Rourke
 #Nov 2024
 
+library(dplyr)
+
 # Set seed for reproducibility
 set.seed(123)
 
@@ -18,11 +20,13 @@ biodiversity_data <- as.data.frame(
 colnames(biodiversity_data) <- species
 rownames(biodiversity_data) <- c("Rocky shores", "Sandy shores", "Mudflats")
 
-# Convert all columns of the data frame to numeric
-biodiversity_data[] <- lapply(biodiversity_data, as.numeric)
+
+# Convert species columns to numeric
+biodiversity_data<-biodiversity_data %>%
+                    mutate_at(c(1:20),as.numeric)
 
 # View the data set
 print(biodiversity_data)
 
 #Save data set as csv
-write.csv(biodiversity_data, "biodiversity_data1.csv", row.names = TRUE)
+write.csv(biodiversity_data, "biodiversity_data.csv", row.names = TRUE)
