b4f9feb0af25bd67558c6f32d0419b92d8d17021
erinor02 <s2214623@ed.ac.uk>
2024-11-27 11:04:12 +0000

    updating data set to make it easier to use

--- Data Code.R
+++ Data Code.R
@@ -5,25 +5,21 @@
 # Set seed for reproducibility
 set.seed(123)
 
-# Simulate species abundance data for 10 samples and 15 species
-samples <- paste0("Site", 1:15)
+# Simulate species abundance data for 3 Habitats and 20 species
+samples <- paste0("Habitat", 1:3)
 species <- paste0("Species", 1:20)
 
+#Create data frame
 biodiversity_data <- as.data.frame(
-  matrix(sample(0:20, 150, replace = TRUE), nrow = 15, ncol = 20)
+  matrix(sample(0:20, 60, replace = TRUE), nrow = 3, ncol = 20)
 )
+
+#Add Column names and row names of each habitat
 colnames(biodiversity_data) <- species
-rownames(biodiversity_data) <- samples
+rownames(biodiversity_data) <- c("Rocky shores", "Sandy shores", "Mudflats")
 
-# Add habitat information (optional)
-biodiversity_data$Habitat <- c("Forest", "Grassland", "Wetland", "Forest",
-                               "Grassland", "Wetland", "Forest", "Grassland",
-                               "Wetland", "Forest","Grassland", "Wetland", "Forest",
-                               "Grassland", "Wetland")
-
-# View the dataset
+# View the data set
 print(biodiversity_data)
 
 #Save data set as csv
-
-write.csv(biodiversity_data, "tutorial data.csv", row.names = FALSE)
+write.csv(biodiversity_data, "biodiversity_data.csv", row.names = FALSE)
